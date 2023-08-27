4f0a3b6efdec755c9a7f56ecd14fa338fd36ed43
J-S-James <jordanjames_tmo@hotmail.com>
2023-08-27 20:35:01 +0100

    Implement CleanArchitecture structure

--- obj/Debug/net7.0-windows/Views/NavigationDrawerView.g.i.cs
+++ obj/Debug/net7.0-windows/Views/NavigationDrawerView.g.i.cs
@@ -56,13 +56,13 @@ namespace WindowsCalculatorClone.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WindowsCalculatorClone;component/views/navigationdrawerview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WindowsCalculatorClone;V1.0.0.0;component/views/navigationdrawerview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\NavigationDrawerView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -72,7 +72,7 @@ namespace WindowsCalculatorClone.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
